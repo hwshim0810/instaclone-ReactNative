edee4d9219998e97e8dccb33c3a6aee4908eb443
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-19 18:26:46 +0900

    Update loginscreen style & reformatting

--- screens/LoginScreen/presenter.js
+++ screens/LoginScreen/presenter.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, Dimensions, TouchableOpacity, TextInput, StatusBar } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  TextInput,
+  StatusBar
+} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 const { width, height } = Dimensions.get('window');
@@ -8,14 +17,30 @@ const LoginScreen = props => (
   <View style={styles.container}>
     <StatusBar barStyle={'light-content'} />
     <View style={styles.header}>
-      <Image source={require('../../assets/images/logo-white.png')} resizeMode="stretch" style={styles.logo} />
+      <Image
+        source={require('../../assets/images/logo-white.png')}
+        resizeMode="stretch"
+        style={styles.logo}
+      />
     </View>
     <View style={styles.content}>
-      <TextInput placeholder="Username" />
-      <TextInput placeholder="Password" />
-      <TouchableOpacity>
-        <View>
-          <Text>Login</Text>
+      <TextInput
+        placeholder="Username"
+        style={styles.textInput}
+        autoCapitalize={'none'}
+        autoCorrect={false}
+        underlineColorAndroid="transparent"
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.textInput}
+        autoCapitalize={'none'}
+        secureTextEntry={true}
+        underlineColorAndroid="transparent"
+      />
+      <TouchableOpacity style={styles.loginTouchable}>
+        <View style={styles.button}>
+          <Text style={styles.btnText}>Login</Text>
         </View>
       </TouchableOpacity>
       <TouchableOpacity style={styles.fbContainer}>
@@ -47,7 +72,7 @@ const styles = StyleSheet.create({
   content: {
     flex: 4,
     backgroundColor: 'white',
-    paddingTop: 20,
+    paddingTop: 50,
     alignItems: 'center',
     justifyContent: 'flex-start'
   },
@@ -62,6 +87,34 @@ const styles = StyleSheet.create({
     color: '#3E99EE',
     marginLeft: 10,
     fontWeight: '600',
+    fontSize: 14
+  },
+  textInput: {
+    height: 50,
+    borderColor: '#bbb',
+    borderWidth: 1,
+    width: width - 80,
+    borderRadius: 5,
+    marginBottom: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FAFAFA',
+    fontSize: 14
+  },
+  loginTouchable: {
+    borderRadius: 5,
+    backgroundColor: '#3E99EE',
+    width: width - 80,
+    marginTop: 25
+  },
+  button: {
+    paddingHorizontal: 7,
+    height: 50,
+    justifyContent: 'center'
+  },
+  btnText: {
+    color: 'white',
+    fontWeight: '600',
+    textAlign: 'center',
     fontSize: 14
   }
 });
