9468b68025afe23cb76c8da78d30fe50364dbaa3
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-19 15:15:46 +0900

    Replace deprecated function at navigatior

--- navigations/LoggedOutNavigation.js
+++ navigations/LoggedOutNavigation.js
@@ -1,7 +1,7 @@
-import { StackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation';
 import LoginScreen from '../screens/LoginScreen';
 
-const LoggedOutNavigation = StackNavigator({
+const LoggedOutNavigation = createStackNavigator({
   Login: {
     screen: LoginScreen
   }
