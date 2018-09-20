04bbfd76f87c860ba862cfb64f2f4e4d8ab9b68e
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-21 04:26:42 +0900

    Create follow functions

--- redux/modules/user.js
+++ redux/modules/user.js
@@ -142,6 +142,50 @@ function getOwnProfile() {
   };
 }
 
+function followUser(userId) {
+  return (dispatch, getState) => {
+    const {
+      user: { token }
+    } = getState();
+    return fetch(`${API_URL}/users/${userId}/follow/`, {
+      method: 'POST',
+      headers: {
+        Authorization: `JWT ${token}`
+      }
+    }).then(response => {
+      if (response.status === 401) {
+        dispatch(logOut());
+      } else if (response.ok) {
+        return true;
+      } else if (!response.ok) {
+        return false;
+      }
+    });
+  };
+}
+
+function unfollowUser(userId) {
+  return (dispatch, getState) => {
+    const {
+      user: { token }
+    } = getState();
+    return fetch(`${API_URL}/users/${userId}/unfollow/`, {
+      method: 'POST',
+      headers: {
+        Authorization: `JWT ${token}`
+      }
+    }).then(response => {
+      if (response.status === 401) {
+        dispatch(logOut());
+      } else if (response.ok) {
+        return true;
+      } else if (!response.ok) {
+        return false;
+      }
+    });
+  };
+}
+
 // Initial State
 
 const initialState = {
@@ -204,7 +248,9 @@ const actionCreators = {
   facebookLogin,
   logout,
   getNotifications,
-  getOwnProfile
+  getOwnProfile,
+  followUser,
+  unfollowUser
 };
 export { actionCreators };
 
