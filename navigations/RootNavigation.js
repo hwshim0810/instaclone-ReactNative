8b9dc569512aa41c2cada994b6643def5313d86d
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-20 14:34:53 +0900

    Add root navigation

---
+++ navigations/RootNavigation.js
@@ -0,0 +1,18 @@
+import { createStackNavigator } from 'react-navigation';
+import TakePhotoScreen from '../screens/TakePhotoScreen';
+
+const RootNavigation = createStackNavigator(
+  {
+    TakePhoto: {
+      screen: TakePhotoScreen,
+      navigationOptions: {
+        header: null
+      }
+    }
+  },
+  {
+    mode: 'modal'
+  }
+);
+
+export default RootNavigation;
