89b7c9092e8c59003b85ae3c7764bb855e978621
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-20 23:18:03 +0900

    Create init function

--- components/AppContainer/index.js
+++ components/AppContainer/index.js
@@ -1,12 +1,21 @@
 import { connect } from 'react-redux';
 import AppContainer from './presenter';
+import { actionCreators as photoActions } from '../../redux/modules/photos';
 
 const mapStateToProps = (state, ownProps) => {
   const { user } = state;
 
   return {
     isLoggedIn: user.isLoggedIn,
-    profile: user.profile,
+    profile: user.profile
+  };
+};
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    initApp: () => {
+      dispatch(photoActions.getFeed());
+    }
   };
 };
 
