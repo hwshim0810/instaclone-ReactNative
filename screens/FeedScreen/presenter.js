4be00766f3f55a2b7edb38ffdd015e4bc1785d28
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-21 00:52:42 +0900

    Connect photo to feed

--- screens/FeedScreen/presenter.js
+++ screens/FeedScreen/presenter.js
@@ -7,6 +7,7 @@ import {
   RefreshControl,
   StyleSheet
 } from 'react-native';
+import Photo from '../../components/Photo';
 
 const FeedScreen = props => (
   <ScrollView
@@ -17,8 +18,12 @@ const FeedScreen = props => (
         tintColor={'black'}
       />
     }
-    contentContainerStyle={styles.container}
-  />
+  >
+    <View style={styles.container}>
+      {props.feed &&
+        props.feed.map(photo => <Photo {...photo} key={photo.id} />)}
+    </View>
+  </ScrollView>
 );
 
 const styles = StyleSheet.create({
