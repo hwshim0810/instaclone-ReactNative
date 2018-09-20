954c831ab0eeb14c4e57e17c19a6fef238dc6a35
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-20 15:08:03 +0900

    Create tab route

--- navigations/TabsNavigation.js
+++ navigations/TabsNavigation.js
@@ -1,11 +1,79 @@
 import React from 'react';
-import { TabNavigator } from 'react-navigation';
+import { View } from 'react-native';
+import { createBottomTabNavigator } from 'react-navigation';
 import { Ionicons } from '@expo/vector-icons';
+import HomeRoute from '../routes/HomeRoute';
+import SearchRoute from '../routes/SearchRoute';
+import ProfileRoute from '../routes/ProfileRoute';
+import NotificationRoute from '../routes/NotificationRoute';
 
-const TabsNavigation = TabNavigator(
-  {},
+const TabsNavigation = createBottomTabNavigator(
   {
-    tabBarPosition: 'bottom'
+    Home: {
+      screen: HomeRoute,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <Ionicons
+            name={focused ? 'ios-home' : 'ios-home-outline'}
+            size={30}
+            color={'black'}
+          />
+        )
+      }
+    },
+    Search: {
+      screen: SearchRoute,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <Ionicons
+            name={focused ? 'ios-search' : 'ios-search-outline'}
+            size={30}
+            color={'black'}
+          />
+        )
+      }
+    },
+    AddPhoto: {
+      screen: View,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <Ionicons name={'ios-add-circle-outline'} size={30} color={'black'} />
+        )
+      }
+    },
+    Notification: {
+      screen: NotificationRoute,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <Ionicons
+            name={focused ? 'ios-heart' : 'ios-heart-outline'}
+            size={30}
+            color={'black'}
+          />
+        )
+      }
+    },
+    Profile: {
+      screen: ProfileRoute,
+      navigationOptions: {
+        tabBarIcon: ({ focused }) => (
+          <Ionicons
+            name={focused ? 'ios-person' : 'ios-person-outline'}
+            size={30}
+            color={'black'}
+          />
+        )
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      showLabel: false,
+      style: {
+        backgroundColor: '#FBFBFB',
+        height: 45
+      }
+    }
   }
 );
 
