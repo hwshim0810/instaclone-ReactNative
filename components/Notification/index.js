cfc3e5505cd4d10268bc705e36c73659e62b0a50
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-21 04:27:04 +0900

    Connect follow function to notification

--- components/Notification/index.js
+++ components/Notification/index.js
@@ -1,4 +1,22 @@
 import { connect } from 'react-redux';
+import { actionCreators as userActions } from '../../redux/modules/user';
 import Container from './container';
 
-export default connect()(Container);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const {
+    creator: { id }
+  } = ownProps;
+  return {
+    followUser: () => {
+      return dispatch(userActions.followUser(id));
+    },
+    unfollowUser: () => {
+      return dispatch(userActions.unfollowUser(id));
+    }
+  };
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Container);
