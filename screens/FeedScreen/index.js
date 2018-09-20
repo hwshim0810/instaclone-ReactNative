e11067c857b4950771fe676325f761fea35e7554
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-20 16:19:30 +0900

    Connect navbutton to sharedRoute

--- screens/FeedScreen/index.js
+++ screens/FeedScreen/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 const FeedScreen = props => (
-  <Text onPress={() => props.navigation.navigate('TakePhoto')}>FeedScreen</Text>
+  <Text onPress={() => props.navigation.navigate('Likes')}>FeedScreen</Text>
 );
 
 export default FeedScreen;
