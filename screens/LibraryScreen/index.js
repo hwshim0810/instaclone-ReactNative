ce856f52a6ab697a06738ab335d4dc6b770ca493
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-23 01:55:11 +0900

    Create photo library screen

--- screens/LibraryScreen/index.js
+++ screens/LibraryScreen/index.js
@@ -1,6 +1,41 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { CameraRoll, Platform } from 'react-native';
+import LibraryScreen from './presenter';
 
-const LibraryScreen = props => <Text>Library</Text>;
+class Container extends Component {
+  state = {
+    photos: null,
+    pickedPhoto: null
+  };
 
-export default LibraryScreen;
+  componentWillMount = async () => {
+    const rollParams = {
+      first: 2000,
+      groupTypes: 'SavedPhotos',
+      assetType: 'Photos'
+    };
+
+    if (Platform.OS == 'android') {
+      delete rollParams.groupTypes;
+    }
+
+    const { edges } = await CameraRoll.getPhotos(rollParams);
+
+    this.setState({
+      photos: edges,
+      pickedPhoto: edges[0]
+    });
+  };
+
+  render() {
+    return <LibraryScreen {...this.state} pickPhoto={this._pickPhoto} />;
+  }
+
+  _pickPhoto = photo => {
+    this.setState({
+      pickedPhoto: photo
+    });
+  };
+}
+
+export default Container;
