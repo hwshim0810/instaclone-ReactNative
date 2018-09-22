c65557420263e85ec558c8aeae6db256dc14bb54
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-23 02:58:16 +0900

    Modified upload function

--- redux/modules/photos.js
+++ redux/modules/photos.js
@@ -146,7 +146,10 @@ function uploadPhoto(file, caption, location, tags) {
     name: `${uuidv1()}.jpg`
   });
   return (dispatch, getState) => {
-    fetch(`${API_URL}/images/`, {
+    const {
+      user: { token }
+    } = getState();
+    return fetch(`${API_URL}/images/`, {
       method: 'POST',
       headers: {
         Authorization: `JWT ${token}`,
@@ -157,6 +160,8 @@ function uploadPhoto(file, caption, location, tags) {
       if (response.status === 401) {
         dispatch(userActions.logOut());
       } else if (response.ok) {
+        dispatch(getFeed());
+        dispatch(userActions.getOwnProfile());
         return true;
       } else {
         return false;
