6be4c959161b3be4e5c04ded22e284d5b0acc827
hyunwoo.shim <shim.hw0810@gmail.com>
2018-09-23 02:10:34 +0900

    Connect route & button

--- screens/LibraryScreen/presenter.js
+++ screens/LibraryScreen/presenter.js
@@ -6,17 +6,26 @@ import {
   Dimensions,
   ScrollView,
   TouchableOpacity,
-  Image
+  Image,
+  StatusBar
 } from 'react-native';
 import FitImage from 'react-native-fit-image';
+import PropTypes from 'prop-types';
+import { MaterialIcons } from '@expo/vector-icons';
 
 const { height, width } = Dimensions.get('window');
 
 const LibraryScreen = props => (
   <View style={styles.container}>
+    <StatusBar hidden={true} />
     {props.photos && (
       <View style={styles.pictureContainer}>
         <FitImage source={{ uri: props.pickedPhoto.node.image.uri }} />
+        <TouchableOpacity onPress={props.approvePhoto}>
+          <View style={styles.action}>
+            <MaterialIcons name="check-circle" color="white" size={40} />
+          </View>
+        </TouchableOpacity>
       </View>
     )}
     {props.photos && (
@@ -44,8 +53,7 @@ const styles = StyleSheet.create({
     flex: 1
   },
   pictureContainer: {
-    flex: 2,
-    justifyContent: 'center'
+    flex: 2
   },
   photos: {
     flex: 1
@@ -57,7 +65,22 @@ const styles = StyleSheet.create({
   smallPhoto: {
     width: width / 3,
     height: width / 3
+  },
+  action: {
+    backgroundColor: 'transparent',
+    height: 40,
+    width: 40,
+    alignSelf: 'flex-end',
+    position: 'absolute',
+    bottom: 10,
+    right: 10
   }
 });
+
+LibraryScreen.propTypes = {
+  pickedPhoto: PropTypes.object,
+  photos: PropTypes.array,
+  approvePhoto: PropTypes.func.isRequired
+};
 
 export default LibraryScreen;
